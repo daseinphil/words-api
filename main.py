54d37c165f2f55d199cd93f0e49e3e8749fbe708
Phil Martin <phillip.martin@asics.com>
2018-09-29 22:07:15 -0400

    homepage change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tornado.web
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello world")
+        self.write("Ssh. You were never here.")
  
 class NounHandler(MainHandler):
     def get(self):
